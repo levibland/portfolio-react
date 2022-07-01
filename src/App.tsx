cc7674c4e706be710c8825481c4fc5b9ce8b920f
levibland <levi_bland22@protonmail.com>
2022-07-01 19:04:19 +1200

    feat: initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>Test</>
+  )
+}
+
+export default App
