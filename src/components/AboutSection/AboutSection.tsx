a57d97198c6f5c14337a4e46f5ef534b39068abe
levibland <levi_bland22@protonmail.com>
2022-07-02 12:20:06 +1200

    feat: add second card group

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -11,11 +11,15 @@ import HomeCards from '../HomeCards';
 import Typescript from '../../assets/tslogo.png';
 import Rust from '../../assets/rustlogo.png';
 import Reactlogo from '../../assets/reactlogo.png';
+import Github from '../../assets/githublogo.png';
+import Linkedin from '../../assets/linkedinlogo.png';
+import Resume from '../../assets/resumelogo.png';
 
 import './styles/AboutSection.scss';
 
 function AboutSection() {
   const aureliusQuote = "\"A man's worth is no greater than his ambitions.\" - ";
+  const senecaQuote = "\"If a man knows not which port he sails, no wind is favorable.\" - ";
 
   const card1 = {
     text: "TypeScript",
@@ -30,6 +34,21 @@ function AboutSection() {
   const card3 = {
     text: "React",
     img: Reactlogo,
+  };
+
+  const card4 = {
+    text: "Github",
+    img: Github,
+  };
+
+  const card5 = {
+    text: "Linkedin",
+    img: Linkedin,
+  };
+
+  const card6 = {
+    text: "Resume",
+    img: Resume,
   }
 
   return (
@@ -42,6 +61,9 @@ function AboutSection() {
             <Quote quote={aureliusQuote} name="Marcus Aurelius" />
             <SectionTitle text="Skills" />
             <HomeCards card1={card1} card2={card2} card3={card3} />
+            <Quote quote={senecaQuote} name="Seneca" />
+            <SectionTitle text="Info" />
+            <HomeCards card1={card4} card2={card5} card3={card6} />
         </div>
     </main>
   );
