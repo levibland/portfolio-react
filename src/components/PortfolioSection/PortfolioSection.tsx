e4dc1ef6c02b17dea7c840ca1e25cba249aa327e
levibland <levi_bland22@protonmail.com>
2022-07-23 11:44:13 +1200

    feat: add subtitle to portfolio page

--- src/components/PortfolioSection/PortfolioSection.tsx
+++ src/components/PortfolioSection/PortfolioSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PrimaryHeader from '../PrimaryHeader';
+import PrimarySub from '../PrimarySub';
 
 import "./styles/PortfolioSection.scss";
 
@@ -9,6 +10,7 @@ function PortfolioSection() {
     <main className="portfolio">
         <div className="wrapper">
             <PrimaryHeader normal="My" pink="Portfolio" />
+            <PrimarySub sentence1={"A collection of my biggest projects"} sentence2={""} />
         </div>
     </main>
   );
